aa84e924b8adca730a5b3074c646305aa5be297f
Harshal Tambat <harshaltambat38@gmail.com>
2023-01-29 16:44:01 +0530

    service and repository layer create

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,9 +1,43 @@
 package com.driver;
 
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Date;
 
 public class Message {
     private int id;
     private String content;
     private Date timestamp;
+
+    public Message() {
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
